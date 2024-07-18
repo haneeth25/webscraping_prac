22bb72d175f946f4a76078b05ebc8d2cadf94307
Haneeth Kosaraju <captaina857@gmail.com>
2024-07-18 07:21:06 +0000

    fetched by going into multiple pages

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -8,11 +8,16 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response):
         books = response.css("article.product_pod")
         for book in books:
-            yield{
-                'name':book.css('h3 a::text').get(),
-                'price':book.css('p.price_color::text').get(),
-                'url':book.css('h3 a::attr(href)').get(),
-            }
+            relative_url = book.css('h3 a::attr(href)').get()
+            if 'catalogue' in relative_url:
+                book_url = 'https://books.toscrape.com/'+relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/'+relative_url
+                
+            yield response.follow(book_url,callback = self.parse_book_page)
+
+
+        
         next_page = response.css('.next a::attr(href)').get()
 
         if next_page is not None:
@@ -22,4 +27,16 @@ class BookspiderSpider(scrapy.Spider):
             else:
                 next_page_url = 'https://books.toscrape.com/catalogue/'+next_page
                 yield response.follow(next_page_url,callback = self.parse)
+
+    
+    def parse_book_page(self,response):
+        table_rows = response.css('.table.table-striped tr')
+        yield{
+            'url':response.url,
+            'title':response.css('.product_main h1::text').get(),
+            'product_type':table_rows[1].css('td::text').get(),
+            'price':table_rows[3].css('td::text').get(),
+        }
+        
+        
         
